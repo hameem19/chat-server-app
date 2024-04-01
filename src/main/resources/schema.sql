7ff4686dea93fa109d6b45e027a6a91bd1fecba7
Hameem Yousuf <31164590+hameem19@users.noreply.github.com>
2024-04-01 12:11:03 +0400

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,16 @@
-CREATE TABLE chat_room (
-                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                           name VARCHAR(255) NOT NULL
+-- Create the chat_room table if it doesn't exist
+CREATE TABLE IF NOT EXISTS chat_room (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE chat_message (
-                              id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                              content TEXT NOT NULL,
-                              created_at TIMESTAMP NOT NULL,
-                              chat_room_id BIGINT NOT NULL,
-                              username VARCHAR(255) NOT NULL,
-                              FOREIGN KEY (chat_room_id) REFERENCES chat_room(id)
+-- Create the chat_message table if it doesn't exist
+CREATE TABLE IF NOT EXISTS chat_message (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    chat_room_id BIGINT NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    deleted BOOLEAN NOT NULL DEFAULT false,
+    FOREIGN KEY (chat_room_id) REFERENCES chat_room(id)
 );
