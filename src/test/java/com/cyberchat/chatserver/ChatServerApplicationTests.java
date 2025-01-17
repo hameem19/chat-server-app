3fe0155ec989999ea424789eb68fee7ee2792ded
Hameem Yousuf <hyousuf@gulfnews.com>
2025-01-17 11:43:11 +0400

    test

--- src/test/java/com/cyberchat/chatserver/ChatServerApplicationTests.java
+++ src/test/java/com/cyberchat/chatserver/ChatServerApplicationTests.java
@@ -3,6 +3,7 @@ package com.cyberchat.chatserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//test
 @SpringBootTest
 class ChatServerApplicationTests {
 
